e8706bfcc2af658a52ac364899c3d9e5e58fb7f6
Yuv Raj <yuvrajthakur90@gmail.com>
2023-07-13 12:49:53 +0530

    new commit

--- app2/app/settings.py
+++ app2/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9&m%e-#bjsutdc8yf#%y#gxt@q+rn1a-)lnq!j-0-)jr*&-oqs'
+SECRET_KEY = 'django-insecure-mz(kqdk-n3h=cyb7)wwy7*=39s-35!6hvdprhaa&u+4$t=02ub'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
